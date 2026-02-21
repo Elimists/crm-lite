bc79c25c9a75fe7a4bd452dd9b5a560d3efb6725
Pran Pandey <pandey.pran@gmail.com>
2026-02-21 15:14:44 -0600

    added health specific model

--- internal/domain/health/service.go
+++ internal/domain/health/service.go
@@ -1,18 +1,5 @@
 package health
 
-// Response is the data structure the API will return.
-// We use JSON tags here so the Service defines the "Contract".
-type Response struct {
-	Name    string `json:"name"`
-	Slug    string `json:"slug"`
-	Version string `json:"version"`
-	Status  string `json:"status"`
-}
-
-type Service interface {
-	GetHealth() Response
-}
-
 type svc struct {
 	name    string
 	slug    string
@@ -27,8 +14,8 @@ func NewService(name, slug, version string) Service {
 	}
 }
 
-func (s *svc) GetHealth() Response {
-	return Response{
+func (s *svc) GetHealth() Health {
+	return Health{
 		Name:    s.name,
 		Slug:    s.slug,
 		Version: s.version,
